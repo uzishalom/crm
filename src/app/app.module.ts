852811a5da1d85e8148fb07b65b4ae15aac5935c
uzishalom <64908639+uzishalom@users.noreply.github.com>
2020-06-02 22:52:33 +0300

    initial DB setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { FirstLetterUperCasePipe } from './pipes/first-letter-uper-case.pipe';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ContentHeaderComponent } from './content-header/content-header.component';
@@ -25,7 +29,14 @@ import { ContactsComponent } from './data-components/contacts/contacts.component
     AddCustomerComponent,
     ContactsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
