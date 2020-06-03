f7242444cd3e686cf6819c360b62ab833e1ae40a
uzishalom <64908639+uzishalom@users.noreply.github.com>
2020-06-03 09:10:09 +0300

    firebaseConfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  firebaseComfig: {},
+  firebaseConfig: {
+    apiKey: 'AIzaSyBtYkfYadXkHnHblesyQG-D1Yj61rGOg4c',
+    authDomain: 'uzicrm-7b3af.firebaseapp.com',
+    databaseURL: 'https://uzicrm-7b3af.firebaseio.com',
+    projectId: 'uzicrm-7b3af',
+    storageBucket: 'uzicrm-7b3af.appspot.com',
+    messagingSenderId: '576178876136',
+    appId: '1:576178876136:web:b0fbb3d36689e261aae35d',
+  },
 };
