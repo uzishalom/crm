5d1c320b2fa94d29ae8f53144e7a75b1ba1f446a
uzishalom <64908639+uzishalom@users.noreply.github.com>
2020-06-04 14:55:33 +0300

    optimization customers async and add loading

--- src/app/data-components/customers/customers.component.ts
+++ src/app/data-components/customers/customers.component.ts
@@ -1,29 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { Customer } from 'src/app/interfaces/customer';
 import { CustomersService } from 'src/app/services/customers.service';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css'],
 })
-export class CustomersComponent implements OnInit, OnDestroy {
-  customers: Observable<Customer[]> = new Observable<Customer[]>();
-  customersCount: number = 0;
-  countSubscription: Subscription;
+export class CustomersComponent {
+  customers: Observable<Customer[]>;
 
   constructor(customersService: CustomersService) {
     this.customers = customersService.getAll();
-  }
-
-  ngOnInit() {
-    this.countSubscription = this.customers.subscribe((data) => {
-      this.customersCount = data.length;
-    });
-  }
-
-  ngOnDestroy() {
-    this.countSubscription.unsubscribe();
   }
 }
