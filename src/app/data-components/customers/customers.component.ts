f99355281a9bd8aa9ac6c729aa3efe467cdc8190
uzishalom <64908639+uzishalom@users.noreply.github.com>
2020-06-04 13:39:55 +0300

    subscribe/unsubscribe count event

--- src/app/data-components/customers/customers.component.ts
+++ src/app/data-components/customers/customers.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Customer } from 'src/app/interfaces/customer';
 import { CustomersService } from 'src/app/services/customers.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css'],
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit, OnDestroy {
   customers: Observable<Customer[]> = new Observable<Customer[]>();
   customersCount: number = 0;
+  countSubscription: Subscription;
 
   constructor(customersService: CustomersService) {
     this.customers = customersService.getAll();
-    this.customers.subscribe((data) => {
+  }
+
+  ngOnInit() {
+    this.countSubscription = this.customers.subscribe((data) => {
       this.customersCount = data.length;
     });
+  }
+
+  ngOnDestroy() {
+    this.countSubscription.unsubscribe();
   }
 }
