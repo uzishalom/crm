a7c6b76a7a7300022fded3f3ffbe6dac571702b6
uzishalom <64908639+uzishalom@users.noreply.github.com>
2020-06-26 10:16:27 +0300

    send-email header + subscribe to customer details

--- src/app/components-data/emails/send-email/send-email.component.ts
+++ src/app/components-data/emails/send-email/send-email.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { Customer } from 'src/app/interfaces/customer';
+import { CustomersService } from 'src/app/services/customers.service';
+import { switchMap, take } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-send-email',
   templateUrl: './send-email.component.html',
   styleUrls: ['./send-email.component.css']
 })
-export class SendEmailComponent implements OnInit {
+export class SendEmailComponent implements OnInit, OnDestroy {
+  customer: Customer;
+  customerFullName: string = "";
+  errorInSending: boolean = false;
+  errorInSaving: boolean = false;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private customersService: CustomersService
+  ) { }
 
   ngOnInit(): void {
+    this.subscription = this.route.params
+      .pipe(
+        switchMap((params) => this.customersService.getById(params.id)),
+        take(1)
+      )
+      .subscribe((customer) => {
+        this.customer = customer;
+        this.customerFullName =
+          this.customer.firstName + ' ' + this.customer.lastName;
+
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
